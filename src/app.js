8d76a4fca526c3d9a3e5500571e9398c3b828726
camila.pereyra <campereyra23051995@gmail.com>
2023-12-03 23:30:51 -0300

    Agregue y configure handlebars. Agregue vista main.handlebars y home.handlebars

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 import express from "express";
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
+import handlebars from "express-handlebars";
+import __dirname from "./utils.js";
+import viewsRouter from "./routes/views.router.js";
 
 const PORT = 8080;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//inicializamos el motor
+app.engine("handlebars", handlebars.engine());
+//indicamos donde estan las vistas
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+//seteamos de manera estatica nuestra carpeta publica
+app.use(express.static(__dirname + "/public"));
 
 app.listen(PORT, () => {
   console.log(`Servidor funcionando en el puerto ${PORT}`);
@@ -15,3 +25,4 @@ app.listen(PORT, () => {
 //Rutas
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
+app.use("/", viewsRouter);
---
+++ src/routes/views.router.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import ProductManager from "../managers/productManager.js";
+const router = Router();
+const productManager = new ProductManager("products.json");
+router.get("/", async (req, res) => {
+  const products = await productManager.getProducts();
+  if (products.length === 0) {
+    res.render("home", { products, existProducts: false });
+  } else {
+    res.render("home", { products, existProducts: true });
+  }
+});
+
+export default router;
